d818bae044a7b115f197083c09a1dbdf165c9d42
Marocs <marcosrlj@outlook.com>
2019-09-04 03:00:15 -0300

    Second codewars exercise. Small length word in string

---
+++ 1-shortestWord.js
@@ -0,0 +1,25 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data 
+types.
+*/
+
+function findShort(s) {
+  listOfWords = s.split(" ");
+  // console.log("words: ", listOfWords);
+  let lengthsList = []
+  listOfWords.map(word => {
+    lengthsList.push(word.length)
+  })
+  // console.log(lengthsList)
+  // console.log(lengthsList.sort(function(a, b){return a-b}))
+  lengthsList.sort(function(a, b){return a-b})
+  let smallestLength = lengthsList[0]
+
+  return smallestLength
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+console.log(findShort("turns out random test cases are easier than writing out basic ones"))
+console.log(findShort("MadeSafeCoin 21inc LiteCoin Ethereum ProofOfWork Lisk Dash Bitcoin Waves Steem Lisk"))
